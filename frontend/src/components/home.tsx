bb7497f4554d3c3b947e67b2905aff12abd12b5e
qt22 <qitian41322@gmail.com>
2022-01-03 14:39:15 -0500

    Create navbar component

--- frontend/src/components/home.tsx
+++ frontend/src/components/home.tsx
@@ -4,12 +4,11 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 
 const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
 }));
-
 
 const Home = () => (
     <Grid container>
